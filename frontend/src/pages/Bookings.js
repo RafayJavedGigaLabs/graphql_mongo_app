99cc478058c577a812fc179a0b5cb199f4b32a68
RafayJavedGigaLabs <rafay.javed@gigalabs.co>
2020-11-26 20:06:53 +0500

    Third Commit 26 Mov 2020

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class BookingsPage extends Component {
+  render() {
+    return <h1>The Bookings Page</h1>;
+  }
+}
+
+export default BookingsPage;
